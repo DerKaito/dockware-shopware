3919b41f4a2829eccbc014fea211d2fb2ef958c6
Christian <boxblinkracer@gmx.net>
2025-12-10 10:18:36 +0100

    improve cypress admin login

--- tests/cypress/cypress/support/actions/AdminActions.js
+++ tests/cypress/cypress/support/actions/AdminActions.js
@@ -5,8 +5,8 @@ export default class AdminActions {
      */
     login() {
         cy.visit('/admin');
-        cy.get('#sw-field--username').type('admin');
-        cy.get('#sw-field--password').type('shopware');
+        cy.get('#sw-field--username', {timeout: 10000}).type('admin');
+        cy.get('#sw-field--password', {timeout: 10000}).type('shopware');
         cy.get('.mt-button').click();
     }
 
